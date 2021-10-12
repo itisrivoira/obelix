9bbe765786511c506b1cc80f74c366cbc2a10f05
icobasco <enrico.allione@gmail.com>
2021-10-12 10:46:42 +0200

    Update config.js
    Aggiunto link per lista_locali_sample_completo.json

--- web/sample_base/js/config.js
+++ web/sample_base/js/config.js
@@ -1,3 +1,4 @@
-let locali_list_json    =   "https://raw.githubusercontent.com/itisrivoira/obelix/main/lista_locali.json";
-let bevande_list_json   =   "https://raw.githubusercontent.com/itisrivoira/obelix/main/lista_bevande_sample.json";
-let cibi_list_json      =   "https://raw.githubusercontent.com/itisrivoira/obelix/main/lista_cibi_sample.json";
+let locali_list_json        =   "https://raw.githubusercontent.com/itisrivoira/obelix/main/lista_locali.json";
+let bevande_list_json       =   "https://raw.githubusercontent.com/itisrivoira/obelix/main/lista_bevande_sample.json";
+let cibi_list_json          =   "https://raw.githubusercontent.com/itisrivoira/obelix/main/lista_cibi_sample.json";
+let locali_fake_list_json   =   "https://raw.githubusercontent.com/itisrivoira/obelix/main/lista_locali_sample_completo.json";
